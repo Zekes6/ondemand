b269b777ec9b1854462640b7a0d257e85a503cac
Karpov, Evgenii <evgenii.karpov@if.lv>
2017-12-04 19:45:32 +0200

    added authentication service example

---
+++ ClientService/Controllers/PaymentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClientService.Managers;
+using ClientService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientService.Controllers
+{
+    [Route("api/[controller]")]
+    public class PaymentController : Controller
+    {
+        private IClientsRepository _clientsRepo { get; set; }
+
+        public PaymentController(IClientsRepository clientsRepository)
+        {
+            _clientsRepo = clientsRepository;
+        }
+
+        [HttpPost]
+        public async Task<Client> AddPayment(string clientId, decimal amount)
+        {
+            var client = await _clientsRepo.Get(clientId);
+
+            if (client == null) return null;
+
+            if (client.PaymentInformation == null)
+            {
+                client.PaymentInformation = new ClientPaymentInformation();
+            }
+
+            client.PaymentInformation.Amount += amount;
+
+            await _clientsRepo.Update(clientId, client);
+
+            return client;
+        }
+    }
+}
