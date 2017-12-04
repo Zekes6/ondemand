fb224fd4ef292b2d8f23d5030b30e3d56a35bcf6
Zubarev Andrey <andrey@zubarev.net>
2017-12-04 21:07:54 +0200

    add ui

---
+++ ui/src/containers/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {View, Text} from 'react-native'
+import {Link} from 'react-router-native'
+
+export default class HomePage extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Home Page</Text>
+                
+            </View>
+        );
+    }
+}
